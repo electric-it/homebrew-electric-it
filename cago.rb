1da691ac8ff4008e864fa7e178e8b3ef40acdbfe
Bernie Durfee <bernie.durfee@ge.com>
2018-09-07 16:05:16 -0400

    A bump

--- cago.rb
+++ cago.rb
@@ -12,9 +12,9 @@
 class Cago < Formula
   desc 'Cago - A manager of AWS profiles'
   homepage 'https://github.com/electric-it/cagophilist'
-  version '2.3.2'
-  url 'https://github.com/electric-it/cagophilist/releases/download/v2.3.2/cago-darwin-amd64-v2.3.2.tar.gz'
-  sha256 "74a20b13d1b7891a4bc31438e064393ec25e80f799406b727bb1bea81e4dd61d"
+  version '2.3.3'
+  url 'https://github.com/electric-it/cagophilist/releases/download/v2.3.3/cago-darwin-amd64-v2.3.3.tar.gz'
+  sha256 "cc833df243a43c90aa0bc7b1993e3e2573bf5ff5d6266e2dcd303357890904ab"
 
   def install
     bin.install "cago"
