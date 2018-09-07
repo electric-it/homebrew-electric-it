cdf563ebbbda2b49658a1cec7d5dacb411fd16e0
Bernie Durfee <bernie.durfee@ge.com>
2018-09-07 13:30:52 -0400

    Bumpity bump

--- cago.rb
+++ cago.rb
@@ -2,12 +2,19 @@
 #                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 
+# READ READ READ READ READ READ READ READ READ READ READ READ READ READ READ READ READ READ READ READ
+#    1. Don't forget to update the version!
+#    2. Don't forget to update the url!
+#    3. Run `openssl sha256 < cago-darwin-amd64-v2.3.2.tar.gz` to get the Hash
+#    4. Don't for get to update the sha256 hash!
+# READ READ READ READ READ READ READ READ READ READ READ READ READ READ READ READ READ READ READ READ
+
 class Cago < Formula
   desc 'Cago - A manager of AWS profiles'
   homepage 'https://github.com/electric-it/cagophilist'
-  version '2.3.1'
-  url 'https://github.com/electric-it/cagophilist/releases/download/v2.3.1/cago-darwin-amd64-v2.3.1.tar.gz'
-  sha256 "e3bee5a58514723155ddcb945287267b59c880657b4774b082cd17cd54c88f44"
+  version '2.3.2'
+  url 'https://github.com/electric-it/cagophilist/releases/download/v2.3.2/cago-darwin-amd64-v2.3.2.tar.gz'
+  sha256 "74a20b13d1b7891a4bc31438e064393ec25e80f799406b727bb1bea81e4dd61d"
 
   def install
     bin.install "cago"
