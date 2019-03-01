3d0b85fe49a960b97c719a2ecfacd71c0f460d2d
Bernie Durfee <bernie.durfee@ge.com>
2019-03-01 14:20:03 -0500

    New version to test

--- cago.rb
+++ cago.rb
@@ -6,14 +6,19 @@ class Cago < Formula
   desc 'Cago - A manager of AWS profiles'
   homepage 'https://github.com/electric-it/cagophilist'
   version '2.3.1'
-  url 'https://github.com/electric-it/cagophilist/releases/download/v2.3.1/cago-darwin-amd64-v2.3.1.tar.gz'
-  sha256 "e3bee5a58514723155ddcb945287267b59c880657b4774b082cd17cd54c88f44"
+
+  stable do
+    url 'https://github.com/electric-it/cagophilist/releases/download/v2.3.1/cago-darwin-amd64-v2.3.1.tar.gz'
+    sha256 'e3bee5a58514723155ddcb945287267b59c880657b4774b082cd17cd54c88f44'
+  end
+
+  devel do
+    url 'https://gitlab.com/electric-it/cago/-/jobs/170262495/artifacts/raw/release/cago-darwin-amd64-v2.4.3.tar.gz'
+    sha256 '9c49000f42eb4b90712604ab0cdf170901ee1b01cb1b2d2a868f2435dfc78610'
+  end
 
   def install
     bin.install "cago"
     bin.install "cago.sh"
-  end
-
-  test do
   end
 end
